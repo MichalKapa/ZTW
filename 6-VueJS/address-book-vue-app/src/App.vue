3d931488299131a33fa700b96f4093339743d8e1
MichalKapa <michal.kapala05@gmail.com>
2023-04-04 12:13:09 +0200

    Base instruction.

--- 6-VueJS/address-book-vue-app/src/App.vue
+++ 6-VueJS/address-book-vue-app/src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div id="app" class="small-container">
+  <h1>Znajomi</h1>
+  <person-form @add:person="addPerson"/>
+  <persons-table :personsSource="persons" />
+  </div>
+ </template>
+ <script>
+   import PersonsTable from '@/components/PersonsTable.vue'
+   import PersonForm from '@/components/PersonForm.vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
+ export default {
+  name: 'app',
   components: {
-    HelloWorld
+    PersonsTable,
+    PersonForm,
+  },
+  data() {
+    return {
+      persons: [
+        // {
+        //   id: 1,
+        //   name: 'Adam Słodowy',
+        //   email: 'adam.slodowy@zrobtosam.pl',
+        //   phone: '+48 787 774 664'
+        // },
+        // {
+        //   id: 2,
+        //   name: 'Michał Studencki',
+        //   email: 'ms@student.pwr.edu.pl',
+        //   phone: '+48 600 565 454'
+        // },
+        // { 
+        //   id: 3,
+        //   name: 'Kamila Napokaz',
+        //   email: 'kami2003@h2.pl',
+        //   phone: '+48 609 554 987'
+        // },
+      ]
+    }
+  }, 
+  methods: {
+    addPerson(person) {
+      this.persons = [...this.persons, person]
+    },
+    async getPersons() {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users')
+          const data = await response.json()
+          this.persons = data
+        } catch (error) {
+          console.error(error)
+        }
+    }, 
+  }, 
+  mounted() {
+    this.getPersons()
+  },
+ } 
+ </script>
+ <style>
+  button {
+  background: #009435;
+  border: 1px solid #009435;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  .small-container {
+  max-width: 680px;
+  }
+  </style>
